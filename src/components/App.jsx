b728853c4e20f11c1f7fc1fb7f62376c2eba3790
onepopina <anyarasp@gmail.com>
2023-05-02 14:30:39 +0200

    add refactoring

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,62 +1,60 @@
-import { Component } from 'react';
+import React, { useState } from 'react';
 import { FeedbackOptions } from './FeedbackOptions/FeedbackOptions';
 import { Section } from './Section/Section';
 import { Statistics } from './Statistics/Statistics';
 import { Notification } from './Notification/Notification';
-import PropTypes from 'prop-types';
 
-export class App extends Component {
-  state = {
-    good: 0,
-    neutral: 0,
-    bad: 0,
+export const Feedback = () => {
+  const [good, setGood] = useState(0);
+  const [neutral, setNeutral] = useState(0);
+  const [bad, setBad] = useState(0);
+
+  const feedback = { good, neutral, bad };
+  const options = Object.keys(feedback);
+
+  const onAddFeedback = statKey => {
+    switch (statKey) {
+      case 'good':
+        setGood(prevState => prevState + 1);
+        break;
+
+      case 'bad':
+        setBad(prevState => prevState + 1);
+        break;
+
+      case 'neutral':
+        setNeutral(prevState => prevState + 1);
+        break;
+
+      default:
+        console.log('error');
+    }
   };
 
-  onAddFeedback = feedbackType => {
-    this.setState(prevState => ({
-      [feedbackType]: prevState[feedbackType] + 1,
-    }));
-  };
-
-  countTotalFeedback = () => {
-    const { good, neutral, bad } = this.state;
+  const countTotalFeedback = () => {
     return good + neutral + bad;
   };
-  countPositiveFeedbackPercentage = () => {
-    const { good } = this.state;
-    return Math.round((good / this.countTotalFeedback()) * 100);
+
+  const countPositiveFeedbackPercentage = () => {
+    return Math.round((good / countTotalFeedback()) * 100);
   };
 
-  render() {
-    return (
-      <div>
-        <Section title="Please leave feedback">
-          <FeedbackOptions
-            options={Object.keys(this.state)}
-            onAddFeedback={this.onAddFeedback}
+  return (
+    <div>
+      <Section title="Please leave feedback">
+        <FeedbackOptions options={options} onAddFeedback={onAddFeedback} />
+      </Section>
+      <Section title="Statistics">
+        {countTotalFeedback() > 0 ? (
+          <Statistics
+            total={countTotalFeedback()}
+            positive={countPositiveFeedbackPercentage()}
+            state={feedback}
           />
-        </Section>
-        <Section title="Statistics">
-          {this.countTotalFeedback() ? (
-            <Statistics
-              good={this.state.good}
-              neutral={this.state.neutral}
-              bad={this.state.bad}
-              count={this.countTotalFeedback()}
-              percentage={this.countPositiveFeedbackPercentage()}
-            />
-          ) : (
-            <Notification message="There is no feedback" />
-          )}
-        </Section>
-      </div>
-    );
-  }
-}
-
-App.propTypes = {
-  good: PropTypes.number.isRequired,
-  neutral: PropTypes.number.isRequired,
-  bad: PropTypes.number.isRequired,
-  onAddFeedback: PropTypes.func.isRequired,
+        ) : (
+          <Notification message="There is no feedback" />
+        )}
+      </Section>
+    </div>
+  );
 };
--- src/components/Statistics/Statistics.jsx
+++ src/components/Statistics/Statistics.jsx
@@ -1,26 +1,27 @@
 import PropTypes from 'prop-types';
 import css from './Statistics.module.css';
-export const Statistics = ({ good, neutral, bad, count, percentage }) => (
+
+export const Statistics = ({ total, positive, state }) => (
   <ul className={css.statList}>
     <li className={css.statItem}>
       <span>Good:</span>
-      <span>{good}</span>
+      <span>{state.good}</span>
     </li>
     <li className={css.statItem}>
       <span>Neutral:</span>
-      <span>{neutral}</span>
+      <span>{state.neutral}</span>
     </li>
     <li className={css.statItem}>
       <span>Bad:</span>
-      <span>{bad}</span>
+      <span>{state.bad}</span>
     </li>
     <li className={css.statItem}>
       <span>Total:</span>
-      <span>{count}</span>
+      <span>{total}</span>
     </li>
     <li className={css.statItem}>
       <span>Positive feedback:</span>
-      <span>{percentage}%</span>
+      <span>{positive}%</span>
     </li>
   </ul>
 );
@@ -29,6 +30,6 @@ Statistics.propTypes = {
   good: PropTypes.number.isRequired,
   neutral: PropTypes.number.isRequired,
   bad: PropTypes.number.isRequired,
-  count: PropTypes.number.isRequired,
-  percentage: PropTypes.number.isRequired,
+  total: PropTypes.number.isRequired,
+  positive: PropTypes.number.isRequired,
 };
